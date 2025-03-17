8ace19b246fe8d2a993fff4b4275e2b725c7dde4
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-03-17 11:59:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import APIAccessControl from './components/APIAccessControl';
 
+function App() {
+  return (
+    <div className="App">
+      <APIAccessControl />
+    </div>
+  );
+}
+
+export default App;
